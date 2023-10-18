8e4f29eb00a3ca4f8fd9e7f336211e183ede1e06
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-10-18 11:11:23 +0500

    added honor data

--- src/pages/Smartfonlar.jsx
+++ src/pages/Smartfonlar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Smartfonlar = () => {
   return (
-    <section className=' bg-white p-5 w-full max-w-sm rounded-lg'>
+    <section className=' bg-white p-5 w-full max-w-sm h-full  rounded-lg'>
       <h2 className='font-semibold text-xl py-4 border-b-2'>Ishlab chiqaruvchi</h2>
       <nav>
         <ul className=' space-y-5  pt-5'>
