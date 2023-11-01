46159f40fd9df425f6ddad2e4c6a781ea2e64e60
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-11-01 22:36:03 +0500

    added cta

---
+++ src/components/Cta.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cta = () => {
+  return (
+    <div className='pt-5'>
+        <h2 className='text-2xl font-semibold py-5'>"Nixol" - xarid qilish uchun qulay kredit only do'kon.</h2>
+        <p className='font-normal text-base'>Har qanday inson uchun qulay hayot va qulay turmush tarzini zamonaviy texnologiyalarsiz tasavvur qilib bo‘lmaydi. Ish va shaxsiy masalalar - smartfonda, sevimli filmlar va teleseriallar - televizor, tozalash - changyutgich, pishirish - pechka, pech va ko'plab kichikroq, ammo foydali qurilmalar. Bundan tashqari, texnologiya rivojlanmoqda, shuning uchun elektronika muntazam yangilanishni talab qiladi; siz yangi modellarni sinab ko'rmoqchisiz yoki mavjud qurilmalarning kuchi shunchaki etarli emas. Shu sababli, maishiy texnika sotib olish uchun ishonchli joyga ega bo'lish foydalidir. Agar siz hali o'zingiz uchun birini topa olmagan bo'lsangiz, sizni veb-saytimizda bir necha daqiqa qolishga taklif qilamiz. Sizni ishontirib aytamizki, vaqtingizni behuda o'tkazmaysiz.</p>
+    </div>
+  )
+}
+
+export default Cta
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Cta from '../components/Cta'
 import MixedData from '../components/MixedData'
 import Swiper from '../components/Swiper'
 
-const Home = ({likedCards, setLikedCards}) => {
+const Home = ({ likedCards, setLikedCards }) => {
   return (
     <div className='main-container'>
       <Swiper />
-     <MixedData likedCards={likedCards} setLikedCards={setLikedCards}/>
+      <MixedData likedCards={likedCards} setLikedCards={setLikedCards} />
+      <Cta />
     </div>
   )
 }
